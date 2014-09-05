bffb4bc399643d330f09354eec60327f5f7ff2f6
Jonas Falck <jonas.falck@gmail.com>
2014-09-06 00:15:45 +0200

    changed websocket lib. built websocket in frontend

--- server/webserver.go
+++ server/webserver.go
@@ -1,22 +1,15 @@
 package main
 
 import (
-	//"code.google.com/p/go.net/websocket"
 	"encoding/json"
-	"fmt"
+	"github.com/beatrichartz/martini-sockets"
 	log "github.com/cihub/seelog"
 	"github.com/go-martini/martini"
-	"github.com/gorilla/websocket"
-	//"github.com/martini-contrib/binding"
 	"github.com/martini-contrib/encoder"
 	"net/http"
 )
 
 // Webserver that serves static files
-var chttp = http.NewServeMux()
-
-var WebSockets map[int]chan string
-var WebSocketPointer int
 
 // The webserver
 type Response map[string]interface{} /*{{{*/
@@ -30,20 +23,15 @@ func (r Response) String() (s string) {
 	return
 }                            /*}}}*/
 func webStart(port string) { /*{{{*/
-	//chttp.Handle("/", http.FileServer(http.Dir("./site/")))
-	//http.HandleFunc("/", webHandler)
-	//http.Handle("/socket", websocket.Handler(socketServer))
 
-	WebSockets = make(map[int]chan string)
 	m := martini.Classic()
-	//m.Use(martini.Static("site"))
 
 	m.Use(func(c martini.Context, w http.ResponseWriter) {
 		c.MapTo(encoder.JsonEncoder{}, (*encoder.Encoder)(nil))
 		w.Header().Set("Content-Type", "application/json; charset=utf-8")
 	})
 
-	m.Get("/socket", socketServer)
+	m.Get("/socket", sockets.JSON(Message{}), websocketRoute)
 	m.Get("/api/nodes", GetNodes)
 	m.Get("/api/node/:id", GetNode)
 	m.Post("/api/node/:id/state", PostNodeState)
@@ -59,85 +47,6 @@ func webStart(port string) { /*{{{*/
 	//go http.ListenAndServe(":"+port, nil)
 	log.Critical(http.ListenAndServe(":"+port, m))
 	//fmt.Println()
-} /*}}}*/
-//func webHandler(w http.ResponseWriter, r *http.Request) { [>{{{<]
-//// Select what to serve
-//if len(r.URL.Path) > 3 && r.URL.Path[1:4] == "api" {
-//handler.ServeHTTP(w, r)
-//return
-//}
-
-//switch r.URL.Path[1:] {
-//case "": // The main page (index.htm)
-//// Handle simulate tag scans
-//if r.FormValue("tag") != "" {
-////
-//}
-
-//// Serve the index.htm
-//body, _ := ioutil.ReadFile("./site/index.htm")
-//fmt.Fprintf(w, string(body))
-//default: // Serve all files in the web folder
-//chttp.ServeHTTP(w, r)
-//}
-//} [>}}}<]
-
-//type T struct {
-//Msg string
-//}
-
-func socketServer(w http.ResponseWriter, r *http.Request) { /*{{{*/
-
-	ws, err := websocket.Upgrade(w, r, nil, 1024, 1024)
-	if _, ok := err.(websocket.HandshakeError); ok {
-		http.Error(w, "Not a websocket handshake", 400)
-		return
-	} else if err != nil {
-		log.Error(err)
-		return
-	}
-	fmt.Printf("jsonServer %#v\n")
-
-	pointer := WebSocketPointer
-	WebSocketPointer++
-
-	WebSockets[pointer] = make(chan string)
-	defer func() { WebSockets[pointer] = nil }()
-	defer close(WebSockets[pointer])
-
-	//var msg T
-
-Main:
-	for {
-		select {
-		case txt := <-WebSockets[pointer]:
-			log.Critical("Worker recived data: ", txt)
-			//websocket.Message.Send(ws, txt)
-			err := ws.WriteMessage(1, []byte(txt))
-			if err != nil {
-				fmt.Println(err)
-				break Main
-			}
-		}
-		//continue
-
-		// Receive receives a text message serialized T as JSON.
-		//_, msg, err := ws.ReadMessage()
-		//if err != nil {
-		//fmt.Println(err)
-		//break
-		//}
-		//fmt.Printf("recv:%#v\n", msg)
-
-		// Send send a text message serialized T as JSON.
-		//err = ws.WriteJSON([]byte(msg))
-		//err = websocket.JSON.Send(ws, msg)
-		//if err != nil {
-		//fmt.Println(err)
-		//break
-		//}
-		//fmt.Printf("send:%#v\n", msg)
-	}
 } /*}}}*/
 
 func GetUser(w *http.ResponseWriter, req *http.Request) {
---
+++ server/websocket.go
@@ -0,0 +1,102 @@
+package main
+
+import (
+	"fmt"
+	"github.com/go-martini/martini"
+	"github.com/gorilla/websocket"
+	"sync"
+)
+
+var clients *Clients
+
+type Message struct {
+	Type string      `json:"type"`
+	Data interface{} `json:"data"`
+}
+type Clients struct {
+	sync.Mutex
+	clients []*Client
+}
+type Client struct {
+	Name       string
+	in         <-chan *Message
+	out        chan<- *Message
+	done       <-chan bool
+	err        <-chan error
+	disconnect chan<- int
+}
+
+// Add a client to a room
+func (r *Clients) appendClient(client *Client) {
+	r.Lock()
+	r.clients = append(r.clients, client)
+	for _, c := range r.clients {
+		if c != client {
+			c.out <- &Message{"status", "Joined this chat"}
+		}
+	}
+	r.Unlock()
+}
+
+func sendOnWs(p martini.Params) {
+	clients.messageOtherClients(&Message{p["action"], nil})
+}
+
+// Message all the other clients
+func (r *Clients) messageOtherClients(msg *Message) {
+	r.Lock()
+	for _, c := range r.clients {
+		c.out <- msg
+	}
+	defer r.Unlock()
+}
+
+// Remove a client
+func (r *Clients) removeClient(client *Client) {
+	r.Lock()
+	defer r.Unlock()
+
+	for index, c := range r.clients {
+		if c == client {
+			r.clients = append(r.clients[:index], r.clients[(index+1):]...)
+		}
+	}
+}
+
+// Remove a client
+func (r *Clients) disconnectAll() {
+	r.Lock()
+	defer r.Unlock()
+	for _, c := range r.clients {
+		c.disconnect <- websocket.CloseGoingAway
+	}
+}
+
+func newClients() *Clients {
+	return &Clients{sync.Mutex{}, make([]*Client, 0)}
+}
+func websocketRoute(params martini.Params, receiver <-chan *Message, sender chan<- *Message, done <-chan bool, disconnect chan<- int, err <-chan error) (int, string) {
+	client := &Client{params["clientname"], receiver, sender, done, err, disconnect}
+	clients.appendClient(client)
+
+	//doPeriodicalStuff()
+	clients.messageOtherClients(&Message{"all", Nodes})
+
+	// A single select can be used to do all the messaging
+	for {
+		select {
+		case <-client.err:
+			// Don't try to do this:
+			// client.out <- &Message{"system", "system", "There has been an error with your connection"}
+			// The socket connection is already long gone.
+			// Use the error for statistics etc
+		case msg := <-client.in:
+			//TODO handle request from websocket frontend here.
+			clients.messageOtherClients(msg)
+		case <-client.done:
+			clients.removeClient(client)
+			fmt.Println("waitgroup DONE")
+			return 200, "OK"
+		}
+	}
+}
