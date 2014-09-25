239f2e50d279a1965dce15f66477d8406bc931de
Jonas Falck <jonas.falck@gmail.com>
2014-09-25 22:07:50 +0200

    cleanup

--- nodes/enocean/main.go
+++ nodes/enocean/main.go
@@ -12,33 +12,16 @@ func main() {
 	send := make(chan *goenocean.Packet)
 	recv := make(chan *goenocean.Packet)
 	goenocean.Serial(send, recv)
-
-	//goenocean.Read(s, processPacket)
 	reciever(recv)
-
 }
 
 func reciever(recv chan *goenocean.Packet) {
 	for {
 		select {
 		case p := <-recv:
+			//fmt.Printf("% x\n", p)
 			fmt.Printf("Packet\t %+v\n", p)
 			fmt.Printf("Header\t %+v\n", p.Header())
 		}
 	}
-}
-
-func processPacket(data []byte) {
-	fmt.Println("raw data")
-	fmt.Println(data)
-	p, err := goenocean.Decode(data)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-
-	fmt.Println(p)
-	fmt.Printf("% x\n", p)
-	fmt.Printf("Packet\t %+v\n", p)
-	fmt.Printf("Header\t %+v\n", p.Header())
 }
