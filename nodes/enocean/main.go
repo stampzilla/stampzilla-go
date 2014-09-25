32ce40503e507196b0864aeab3c1e73b9f4cce59
Jonas Falck <jonas.falck@gmail.com>
2014-09-25 19:19:39 +0200

    changed to channel

--- nodes/enocean/main.go
+++ nodes/enocean/main.go
@@ -3,28 +3,29 @@ package main
 
 import (
 	"fmt"
-	"log"
 
 	"github.com/jonaz/goenocean"
-	"github.com/tarm/goserial"
 )
 
 func main() {
 
-	c := &serial.Config{Name: "/dev/ttyUSB0", Baud: 57600}
-	s, err := serial.OpenPort(c)
-	if err != nil {
-		log.Fatal(err)
+	send := make(chan *goenocean.Packet)
+	recv := make(chan *goenocean.Packet)
+	goenocean.Serial(send, recv)
+
+	//goenocean.Read(s, processPacket)
+	reciever(recv)
+
+}
+
+func reciever(recv chan *goenocean.Packet) {
+	for {
+		select {
+		case p := <-recv:
+			fmt.Printf("Packet\t %+v\n", p)
+			fmt.Printf("Header\t %+v\n", p.Header())
+		}
 	}
-
-	//buf := make([]byte, 5000)
-	//s.Read(buf)
-	//fmt.Println(buf)
-
-	//return
-
-	goenocean.Read(s, processPacket)
-
 }
 
 func processPacket(data []byte) {
