998fadd47713b94e7c18edab960cf6daa92de3ff
Jonas Falck <jonas.falck@gmail.com>
2014-09-27 00:57:13 +0200

    work on enocean

--- nodes/enocean/main.go
+++ nodes/enocean/main.go
@@ -9,19 +9,26 @@ import (
 
 func main() {
 
-	send := make(chan *goenocean.Packet)
-	recv := make(chan *goenocean.Packet)
+	send := make(chan goenocean.PacketInterface)
+	recv := make(chan goenocean.PacketInterface)
 	goenocean.Serial(send, recv)
 	reciever(recv)
 }
 
-func reciever(recv chan *goenocean.Packet) {
+func reciever(recv chan goenocean.PacketInterface) {
 	for {
 		select {
 		case p := <-recv:
-			//fmt.Printf("% x\n", p)
+			fmt.Printf("% x\n", p)
 			fmt.Printf("Packet\t %+v\n", p)
 			fmt.Printf("Header\t %+v\n", p.Header())
+			fmt.Printf("senderID: % x\n", p.SenderId())
+
+			if b, ok := p.(*goenocean.PacketEepRps); ok {
+				fmt.Printf("Action: %d\n", b.Action())
+				fmt.Printf("Action: %b\n", b.Action())
+				fmt.Printf("Action: %s\n", b.Action())
+			}
 		}
 	}
 }
