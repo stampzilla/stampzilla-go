410b97b590ef2a62074d6817e7f65b5cd20f9693
Jonas Falck <jonas.falck@gmail.com>
2014-09-28 18:39:39 +0200

    trying to send telegram

--- nodes/enocean/main.go
+++ nodes/enocean/main.go
@@ -9,13 +9,27 @@ import (
 
 func main() {
 
-	send := make(chan goenocean.PacketInterface)
-	recv := make(chan goenocean.PacketInterface)
+	send := make(chan goenocean.Packet)
+	recv := make(chan goenocean.Packet)
 	goenocean.Serial(send, recv)
+
+	testSend(send)
 	reciever(recv)
 }
 
-func reciever(recv chan goenocean.PacketInterface) {
+func testSend(send chan goenocean.Packet) {
+	p := goenocean.NewTelegramRps()
+	p.SetSenderId([4]byte{0xfe, 0xfe, 0x74, 0x9b}) //the hardcoded senderid of my PTM215 button
+	//p.SetTelegramData(0x70) //off
+	p.SetTelegramData(0x50) //on
+	//p.SetStatus(0x30)
+
+	fmt.Printf("sending: % x\n", p.Encode())
+	send <- p
+
+}
+
+func reciever(recv chan goenocean.Packet) {
 	for {
 		select {
 		case p := <-recv:
@@ -24,11 +38,12 @@ func reciever(recv chan goenocean.PacketInterface) {
 			fmt.Printf("Header\t %+v\n", p.Header())
 			fmt.Printf("senderID: % x\n", p.SenderId())
 
-			if b, ok := p.(*goenocean.PacketEepRps); ok {
+			if b, ok := p.(*goenocean.TelegramRps); ok {
 				fmt.Printf("Action: %d\n", b.Action())
 				fmt.Printf("Action: %b\n", b.Action())
 				fmt.Printf("Action: %s\n", b.Action())
 			}
+
 		}
 	}
 }
