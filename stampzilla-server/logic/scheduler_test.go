1ea852ad8c40250b8f59bbebba3810e4eb18353f
Jonas Falck <jonas.falck@gmail.com>
2014-12-04 10:59:28 +0100

    created test for scheduler.RemoveTask

--- stampzilla-server/logic/scheduler_test.go
+++ stampzilla-server/logic/scheduler_test.go
@@ -31,3 +31,36 @@ func TestSchedulerAddTask(t *testing.T) {
 	}
 	t.Errorf("actionRunCount wrong expected cron to have ran 4 times got %d", actionRunCount)
 }
+func TestSchedulerRemoveTask(t *testing.T) {
+
+	scheduler := NewScheduler()
+	actionRunCount := 0
+	action := NewRuleActionStub(&actionRunCount)
+
+	//Add first task.
+	task := scheduler.AddTask("Test1")
+	task.AddAction(action)
+	task.Schedule("* * * * * *")
+	uuid1 := task.Uuid()
+
+	//Add a second task.
+	task = scheduler.AddTask("Test2")
+	task.AddAction(action)
+	task.Schedule("* * * * * *")
+	uuid2 := task.Uuid()
+
+	//Start Cron
+	scheduler.Cron.Start()
+
+	if len(scheduler.Cron.Entries()) != 2 {
+		t.Errorf("expected 2 cron Entries. Got: %d", len(scheduler.Cron.Entries()))
+	}
+	scheduler.RemoveTask(uuid1)
+	if len(scheduler.Cron.Entries()) != 1 {
+		t.Errorf("expected 1 cron Entries. Got: %d", len(scheduler.Cron.Entries()))
+	}
+	scheduler.RemoveTask(uuid2)
+	if len(scheduler.Cron.Entries()) != 0 {
+		t.Errorf("expected 0 cron Entries. Got: %d", len(scheduler.Cron.Entries()))
+	}
+}
