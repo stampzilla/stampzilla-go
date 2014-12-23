3bf6fc00b6e1009b972a55b1e8a15da9f0cac105
Jonas Falck <jonas.falck@gmail.com>
2014-12-23 10:27:33 +0100

    fix router test

--- stampzilla-server/websocket/router_test.go
+++ stampzilla-server/websocket/router_test.go
@@ -1,23 +1,17 @@
 package websocket
 
-import (
-	"encoding/json"
-	"testing"
-)
+import "testing"
 
 func TestRouterAddRoute(t *testing.T) {
 
-	handler := func(msg string) {
+	handler := func(msg *Message) {
 	}
 
 	r := NewRouter()
 	r.AddRoute("cmd", handler)
 
-	str, err := json.Marshal(&Message{Type: "cmd"})
-	if err != nil {
-		t.Error(err)
-	}
-	if r.Run(string(str)) == nil {
+	msg := &Message{Type: "cmd"}
+	if r.Run(msg) == nil {
 		return
 	}
 	t.Error("Route handle for cmd not found")
