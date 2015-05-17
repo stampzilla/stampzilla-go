07753d0c9c8e10e3fbfafb05b421945acf1a487f
stamp <stamp@stamp.se>
2015-05-17 11:37:59 +0200

    Added new status fields and command capabilites

--- nodes/stampzilla-aquarium/main.go
+++ nodes/stampzilla-aquarium/main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"io"
+	"reflect"
 	"strconv"
 	"strings"
 	"time"
@@ -23,15 +25,17 @@ type SerialConnection struct {
 var node *protocol.Node
 var state *State = &State{}
 var serverConnection *basenode.Connection
-var c0 *SerialConnection
+var ard *SerialConnection
+var disp *SerialConnection
 
 func main() {
 	config := basenode.NewConfig()
 	basenode.SetConfig(config)
 
-	var dev string
-	flag.StringVar(&dev, "dev", "/dev/arduino", "Arduino serial port")
-	flag.Parse()
+	var arddev string
+	var dispdev string
+	flag.StringVar(&arddev, "arduino-dev", "/dev/arduino", "Arduino serial port")
+	flag.StringVar(&dispdev, "display-dev", "/dev/poleDisplay", "Display serial port")
 
 	flag.Parse()
 
@@ -54,13 +58,141 @@ func main() {
 	go serverRecv(serverConnection)
 
 	// Setup the serial connection
-	c0 = &SerialConnection{Name: dev, Baud: 9600}
-	c0.connect(serverConnection)
+	ard = &SerialConnection{Name: arddev, Baud: 115200}
+	ard.run(serverConnection, func(data string, connection *basenode.Connection) {
+		processArduinoData(data, connection)
+	})
 
+	disp = &SerialConnection{Name: dispdev, Baud: 9600}
+	disp.run(serverConnection, func(data string, connection *basenode.Connection) {
+		log.Debug("Incomming from display", data)
+	})
+
+	go updateDisplay(serverConnection, disp)
+
+	select {}
 }
 
+var updateInhibit bool = false
+var changed bool = false
+
+func processArduinoData(msg string, connection *basenode.Connection) { // {{{
+	log.Debug(msg)
+
+	var prevState State = *state
+
+	values := strings.Split(msg, "|")
+	if len(values) != 6 {
+		return
+	}
+
+	// Temperature// {{{
+	value, err := strconv.ParseFloat(values[0], 32)
+	if err != nil {
+		return
+	}
+	//if value != state.WaterTemperature {
+	//changed = true
+	//}
+	state.WaterTemperature = value // }}}
+
+	// Filling stat0e // {{{
+	value, err = strconv.ParseFloat(values[1], 32)
+	if err != nil {
+		return
+	}
+	//if state.FillingTime != value {
+	//changed = true
+	//}
+	state.FillingTime = value // }}}
+
+	// Cooling %// {{{
+	value, err = strconv.ParseFloat(values[2], 32)
+	if err != nil {
+		return
+	}
+	//if state.Cooling != value {
+	//changed = true
+	//}
+	state.Cooling = value // }}}
+
+	bits := values[3][0]
+
+	state.CirculationPumps = bits&0x01 > 0
+	state.Skimmer = bits&0x02 > 0
+	state.Heating = bits&0x04 > 0
+	state.Filling = bits&0x08 == 0
+	state.WaterLevelOk = bits&0x0F == 0
+	state.FilterOk = bits&0x10 == 0
+
+	// Check if something have changed
+	changed = reflect.DeepEqual(prevState, *state)
+
+	if !updateInhibit && changed {
+		changed = false
+		connection.Send <- node.Node()
+		updateInhibit = true
+		//log.Warn("Invalid pacakge: ", msg)
+		go func() {
+			<-time.After(time.Millisecond * 100)
+			updateInhibit = false
+		}()
+	}
+}                                                                               // }}}
+func updateDisplay(connection *basenode.Connection, serial *SerialConnection) { // {{{
+	for {
+		select {
+		case <-time.After(time.Second / 15):
+			if serial.Port == nil {
+				continue
+			}
+
+			var msg, row1, row2 string
+			var wLevel string = "OK"
+
+			//if !state.WaterLevelOk {
+			//wLevel = "WLVL"
+			//}
+			if state.FillingTime > 0 {
+				wLevel = "FILL ERR"
+				if state.FillingTime < 10000 {
+					wLevel = "FILLING"
+				}
+			}
+
+			//msg += "\x1B" // Reset
+			//msg += "\x0E" // Clear
+			//msg += "\x13" // Cursor on
+			msg += "\x11" // Character over write mode
+			//msg += "\x12" // Vertical scroll mode
+			//msg += "\x13" // Cursor on
+			msg += "\x14" // Cursor off
+
+			msg += "\r"
+
+			row1 = time.Now().Format("15:04:05") + strings.Repeat(" ", 12-len(wLevel)) + wLevel
+			row2 = "Temp " + strconv.FormatFloat(state.WaterTemperature, 'f', 2, 64) + " Cool " + strconv.FormatFloat(state.Cooling, 'f', 2, 64)
+
+			if 20-len(row1) > 0 {
+				row1 += strings.Repeat(" ", 20-len(row1))
+			}
+
+			if 20-len(row2) > 0 {
+				row2 += strings.Repeat(" ", 20-len(row2))
+			}
+			msg += row1[0:20]
+			msg += row2[0:20]
+
+			_, err := serial.Port.Write([]byte(msg))
+			if err != nil {
+				log.Debug("Failed write to display:", err)
+			}
+		}
+	}
+} // }}}
+
 // WORKER that monitors the current connection state
-func monitorState(connection *basenode.Connection) {
+func monitorState(connection *basenode.Connection) { // {{{
 	for s := range connection.State {
 		switch s {
 		case basenode.ConnectionStateConnected:
@@ -68,107 +200,117 @@ func monitorState(connection *basenode.Connection) {
 		case basenode.ConnectionStateDisconnected:
 		}
 	}
-}
+} // }}}
 
 // WORKER that recives all incomming commands
-func serverRecv(connection *basenode.Connection) {
+func serverRecv(connection *basenode.Connection) { // {{{
 	for d := range connection.Receive {
 		if err := processCommand(d); err != nil {
 			log.Error(err)
 		}
 	}
-}
+} // }}}
 
-func processCommand(cmd protocol.Command) error {
+func processCommand(cmd protocol.Command) error { // {{{
+	if len(cmd.Args) < 1 {
+		return fmt.Errorf("Missing arguments, ignoring command")
+	}
 
+	switch cmd.Cmd {
+	case "CirculationPumps":
+		if cmd.Args[0] != "" && cmd.Args[0] != "0" {
+			ard.Port.Write([]byte{0x02, 0x01, 0x01, 0x03}) // Turn on
+		} else {
+			ard.Port.Write([]byte{0x02, 0x01, 0x00, 0x03}) // Turn off
+		}
+		break
+	case "Skimmer":
+		if cmd.Args[0] != "" && cmd.Args[0] != "0" {
+			ard.Port.Write([]byte{0x02, 0x02, 0x01, 0x03}) // Turn on
+		} else {
+			ard.Port.Write([]byte{0x02, 0x02, 0x00, 0x03}) // Turn off
+		}
+		break
+	case "Heating":
+		if cmd.Args[0] != "" && cmd.Args[0] != "0" {
+			ard.Port.Write([]byte{0x02, 0x03, 0x01, 0x03}) // Turn on
+		} else {
+			ard.Port.Write([]byte{0x02, 0x03, 0x00, 0x03}) // Turn off
+		}
+		break
+	case "CoolingP":
+		i, err := strconv.Atoi(cmd.Args[0])
+		if err != nil {
+			return fmt.Errorf("Failed to decode arg[0] to int %s %s", err, cmd.Args[0])
+		}
+
+		ard.Port.Write([]byte{0x02, 0x04, byte(i), 0x03}) // Turn on
+		break
+	case "CoolingI":
+		i, err := strconv.Atoi(cmd.Args[0])
+		if err != nil {
+			return fmt.Errorf("Failed to decode arg[0] to int %s %s", err, cmd.Args[0])
+		}
+
+		ard.Port.Write([]byte{0x02, 0x05, byte(i), 0x03}) // Turn on
+		break
+	case "CoolingD":
+		i, err := strconv.Atoi(cmd.Args[0])
+		if err != nil {
+			return fmt.Errorf("Failed to decode arg[0] to int %s %s", err, cmd.Args[0])
+		}
+
+		ard.Port.Write([]byte{0x02, 0x06, byte(i), 0x03}) // Turn on
+		break
+	}
 	return nil
-}
+} // }}}
 
-func (config *SerialConnection) connect(connection *basenode.Connection) {
+// Serial connection workers
+func (config *SerialConnection) run(connection *basenode.Connection, callback func(data string, connection *basenode.Connection)) { // {{{
+	go func() {
+		for {
+			config.connect(connection, callback)
+			<-time.After(time.Second * 4)
+		}
+	}()
+}                                                                                                                                       // }}}
+func (config *SerialConnection) connect(connection *basenode.Connection, callback func(data string, connection *basenode.Connection)) { // {{{
 
 	c := &serial.Config{Name: config.Name, Baud: config.Baud}
 	var err error
 
 	config.Port, err = serial.OpenPort(c)
 	if err != nil {
-		log.Critical(err)
+		log.Error("Serial connect failed: ", err)
 		return
 	}
 
-	go func() {
-		var incomming string = ""
-		var updateInhibit bool = false
-		var changed bool = false
+	var incomming string = ""
+
+	for {
+		buf := make([]byte, 128)
+
+		n, err := config.Port.Read(buf)
+		if err != nil {
+			log.Error("Serial read failed: ", err)
+			return
+		}
+
+		incomming += string(buf[:n])
 
 		for {
-			buf := make([]byte, 128)
+			n := strings.Index(incomming, "\r")
 
-			n, err := config.Port.Read(buf)
-			if err != nil {
-				log.Critical(err)
-				return
+			if n < 0 {
+				break
 			}
 
-			incomming += string(buf[:n])
+			msg := strings.TrimSpace(incomming[:n])
+			incomming = incomming[n+1:]
+			//log.Info(msg)
 
-			for {
-				n := strings.Index(incomming, "\r")
-
-				if n < 0 {
-					break
-				}
-
-				msg := strings.TrimSpace(incomming[:n])
-				incomming = incomming[n+1:]
-				//log.Info(msg)
-
-				values := strings.Split(msg, "|")
-				if len(values) != 3 {
-					continue
-				}
-
-				value, err := strconv.ParseFloat(values[0], 32)
-				if err != nil {
-					continue
-				}
-				if float32(value) != state.WaterTemperature {
-					changed = true
-				}
-				state.WaterTemperature = float32(value)
-
-				value, err = strconv.ParseFloat(values[1], 32)
-				if err != nil {
-					continue
-				}
-				if state.Filling != (value > 0) {
-					changed = true
-
-				}
-				state.Filling = value > 0
-
-				value, err = strconv.ParseFloat(values[2], 32)
-				if err != nil {
-					continue
-				}
-				if state.Cooling != float32(value) {
-					changed = true
-				}
-				state.Cooling = float32(value)
-
-				if !updateInhibit && changed {
-					changed = false
-					connection.Send <- node.Node()
-					updateInhibit = true
-					//log.Warn("Invalid pacakge: ", msg)
-					go func() {
-						<-time.After(time.Millisecond * 100)
-						updateInhibit = false
-					}()
-				}
-
-			}
+			go callback(msg, connection)
 		}
-	}()
-
-	select {}
-}
+	}
+} // }}}
--- nodes/stampzilla-aquarium/state.go
+++ nodes/stampzilla-aquarium/state.go
@@ -1,13 +1,23 @@
 package main
 
 type State struct { /*{{{*/
-	WaterTemperature float32
-	Cooling          float32
-	Filling          bool
-	WaterLevelOk     bool
+	WaterTemperature float64
+	Cooling          float64
+	Heating          bool
+
+	Skimmer          bool
+	CirculationPumps bool
+
+	Filling     bool
+	FillingTime float64
+
+	WaterLevelOk bool
+	FilterOk     bool
 
 	Lights struct {
 		White       float32
+		Red         float32
+		Greed       float32
 		Blue        float32
 		Temperature float32
 	}
