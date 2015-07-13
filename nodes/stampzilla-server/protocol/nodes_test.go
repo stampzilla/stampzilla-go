04b2533a0b3b86c1fd36556e1e30d893e2f2da81
Jonas Falck <jonas.falck@gmail.com>
2015-07-13 11:28:07 +0200

    added test for nodes write and delete

--- nodes/stampzilla-server/protocol/nodes_test.go
+++ nodes/stampzilla-server/protocol/nodes_test.go
@@ -1,6 +1,10 @@
 package protocol
 
-import "testing"
+import (
+	"bufio"
+	"net"
+	"testing"
+)
 
 func TestSearch(t *testing.T) {
 	nodes := NewNodes()
@@ -26,4 +30,40 @@ func TestSearch(t *testing.T) {
 		t.Errorf("nodes.Search expected: nil got: %s", found.Name)
 	}
 
+}
+func TestDelete(t *testing.T) {
+	nodes := NewNodes()
+	node := &Node{}
+	node.Name = "Test"
+	node.Uuid = "testuuid"
+	nodes.Add(node)
+
+	nodes.Delete("testuuid")
+
+	if len(nodes.All()) != 0 {
+		t.Error("expected nodes length to be 0")
+	}
+
+}
+
+func TestWrite(t *testing.T) {
+	cli, srv := net.Pipe()
+	defer cli.Close()
+
+	node := &Node{}
+	node.Name = "Test"
+	node.Uuid = "testuuid"
+	node.SetConn(cli)
+
+	go func() {
+		node.Write([]byte("hej"))
+	}()
+	status, err := bufio.NewReader(srv).ReadString('\n')
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	if status != "hej\n" {
+		t.Error("Expected to have read \"hej\" got: ", status)
+	}
 }
