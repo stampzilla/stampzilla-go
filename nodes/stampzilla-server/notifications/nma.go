5828502348900054a0f620da161567671b14ba27
stamp <stamp@stamp.se>
2015-08-21 10:21:38 -0700

    Changed to lib github.com/dustin/go-nma

--- nodes/stampzilla-server/notifications/nma.go
+++ nodes/stampzilla-server/notifications/nma.go
@@ -1,34 +1,45 @@
 package notifications
 
 import (
-	"io/ioutil"
-	"net/http"
-	"net/url"
 	"sync"
 
 	log "github.com/cihub/seelog"
+	"github.com/dustin/go-nma"
 )
 
 // NMA - NotifyMyAndroid
 type Nma struct {
 	wg     sync.WaitGroup
+	nma    *nma.NMA
 	ApiKey string
 }
 
 func (self *Nma) Start() {
+	self.nma = nma.New(self.ApiKey)
 }
 func (self *Nma) Dispatch(note Notification) {
 	self.wg.Add(1)
 	defer self.wg.Done()
 
-	resp, err := http.PostForm("https://www.notifymyandroid.com/publicapi/notify", url.Values{"apikey": {self.ApiKey}, "application": {"Stampzilla"}, "event": {note.Message}, "description": {note.Source}})
-	if err != nil {
-		log.Warn("Failed to deliver notification: ", err)
+	log.Trace("Nma deliver start")
+	e := nma.Notification{
+		Event:       note.Message,
+		Description: note.Source,
+		Priority:    1,
 	}
-	defer resp.Body.Close()
-	body, err := ioutil.ReadAll(resp.Body)
 
-	log.Trace("Nma deliver result: ", string(body))
+	if err := self.nma.Notify(&e); err != nil {
+		log.Error("Error sending message:  %v\n", err)
+	}
+
+	//resp, err := http.PostForm("https://www.notifymyandroid.com/publicapi/notify", url.Values{"apikey": {self.ApiKey}, "application": {"Stampzilla"}, "event": {note.Message}, "description": {note.Source}})
+	//if err != nil {
+	//log.Warn("Failed to deliver notification: ", err)
+	//}
+	//defer resp.Body.Close()
+	//body, err := ioutil.ReadAll(resp.Body)
+
+	log.Trace("Nma deliver done")
 
 }
 func (self *Nma) Stop() {
