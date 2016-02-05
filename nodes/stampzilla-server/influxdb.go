50e9971558536d46ff4247ad4faca7efc2b8db48
Jonas Falck <jonas.falck@gmail.com>
2016-02-06 00:40:35 +0100

    Update to latest influxdb client lib

--- nodes/stampzilla-server/influxdb.go
+++ nodes/stampzilla-server/influxdb.go
@@ -2,48 +2,37 @@ package main
 
 import (
 	"fmt"
-	"net/url"
 	"time"
 
 	log "github.com/cihub/seelog"
-	"github.com/influxdb/influxdb/client"
+	client "github.com/influxdb/influxdb/client/v2"
 	serverprotocol "github.com/stampzilla/stampzilla-go/nodes/stampzilla-server/protocol"
 )
 
 type InfluxDb struct {
 	Config *ServerConfig         `inject:""`
 	Nodes  *serverprotocol.Nodes `inject:""`
-	conn   *client.Client
+	conn   client.Client
 }
 
 func NewInfluxDb() *InfluxDb {
 	return &InfluxDb{}
 }
 
-func (i *InfluxDb) Connect() *client.Client {
+func (i *InfluxDb) Connect() client.Client {
 
-	u, err := url.Parse(fmt.Sprintf("http://%s:8086", i.Config.InfluxDbServer))
-	if err != nil {
-		log.Error(err)
-	}
-
-	conf := client.Config{
-		URL:      *u,
+	conf := client.HTTPConfig{
+		Addr:     fmt.Sprintf("http://%s:8086", i.Config.InfluxDbServer),
 		Username: i.Config.InfluxDbUser,
 		Password: i.Config.InfluxDbPassword,
 	}
 
-	con, err := client.NewClient(conf)
+	con, err := client.NewHTTPClient(conf)
 	if err != nil {
 		log.Error(err)
 	}
 
-	dur, ver, err := con.Ping()
-	if err != nil {
-		log.Error(err)
-	}
-	log.Infof("Connected to influxdb: %v, %s", dur, ver)
-
+	log.Infof("Connected to influxdb: %s", conf.Addr)
 	return con
 }
 
@@ -52,26 +41,32 @@ func (self *InfluxDb) Start() {
 }
 
 func (self *InfluxDb) Log(key string, value interface{}) {
-	var pts = make([]client.Point, 1)
 	log.Trace("Logging: ", key, " = ", value)
-	pts[0] = client.Point{
-		Measurement: key,
-		//Tags: map[string]string{
-		//"color": "test",
-		//"shape": "test",
-		//},
-		Fields: map[string]interface{}{
+
+	pt, err := client.NewPoint(
+		key,
+		nil,
+		map[string]interface{}{
 			"value": value,
 		},
-		Time: time.Now(),
-		//Precision: "s",
+		time.Now(),
+	)
+	if err != nil {
+		log.Error(err)
+		return
 	}
 
-	bps := client.BatchPoints{
-		Points:   pts,
-		Database: "stampzilla",
+	bp, err := client.NewBatchPoints(client.BatchPointsConfig{
+		Database:  "stampzilla",
+		Precision: "s",
+	})
+	if err != nil {
+		log.Error(err)
+		return
 	}
-	_, err := self.conn.Write(bps)
+
+	bp.AddPoint(pt)
+	err = self.conn.Write(bp)
 	if err != nil {
 		log.Error(err)
 	}
