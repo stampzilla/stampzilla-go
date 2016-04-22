1777e65789388cf7699da1fecb755863ad527f4f
Jonas Falck <jonas.falck@gmail.com>
2016-04-22 22:55:09 +0200

    Update volume and status etc

--- nodes/stampzilla-chromecast/chromecast.go
+++ nodes/stampzilla-chromecast/chromecast.go
@@ -9,18 +9,19 @@ import (
 )
 
 type Chromecast struct {
-	Id string
+	Id    string
+	Name_ string `json:"Name"`
 
 	PrimaryApp      string
 	PrimaryEndpoint string
 	//PlaybackActive  bool
 	//Paused          bool
 
-	//IsStandBy     bool
-	//IsActiveInput bool
+	IsStandBy     bool
+	IsActiveInput bool
 
-	//Volume float64
-	//Muted  bool
+	Volume float64
+	Muted  bool
 
 	Addr net.IP
 	Port int
@@ -49,9 +50,10 @@ func (c *Chromecast) Event(event events.Event) {
 	case events.Connected:
 		log.Info(c.Name(), "- Connected, weeihoo")
 
-		c.Addr = c.Device.Ip()
+		c.Addr = c.Ip()
 		c.Port = c.Device.Port()
-		c.Id = c.Device.Uuid()
+		c.Id = c.Uuid()
+		c.Name_ = c.Name()
 
 		state.Add(c)
 	case events.Disconnected:
@@ -68,6 +70,13 @@ func (c *Chromecast) Event(event events.Event) {
 	case events.AppStopped:
 		c.PrimaryApp = ""
 		log.Info(c.Name(), "- App stopped:", data.DisplayName, "(", data.AppID, ")")
+
+	case events.ReceiverStatus:
+		c.IsStandBy = data.Status.IsStandBy
+		c.IsActiveInput = data.Status.IsActiveInput
+		c.Volume = data.Status.Volume.Level
+		c.Muted = data.Status.Volume.Muted
+
 	//gocast.MediaEvent:
 	default:
 		log.Warn("unexpected event %T: %#v\n", data, data)
