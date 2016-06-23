c27dbce59a87dcc5ace4b66746a3d3b9ec14e936
stamp <stamp@stamp.se>
2016-06-23 22:44:39 +0200

    Added broadcast-address option

--- nodes/stampzilla-lifx/main.go
+++ nodes/stampzilla-lifx/main.go
@@ -22,6 +22,10 @@ var serverSendChannel chan interface{}
 var serverRecvChannel chan protocol.Command
 var lifxClient *client.Client
 
+type Config struct {
+	BroadcastAddress string `json:"broadcastAddress"`
+}
+
 func main() { // {{{
 	// Parse all commandline arguments, host and port parameters are added in the basenode init function
 	flag.Parse()
@@ -31,6 +35,12 @@ func main() { // {{{
 
 	//Activate the config
 	basenode.SetConfig(config)
+	nc := &Config{}
+	err := config.NodeSpecific(&nc)
+	if err != nil {
+		log.Error(err)
+		return
+	}
 
 	node = protocol.NewNode("lifx")
 
@@ -47,8 +57,11 @@ func main() { // {{{
 	state = NewState()
 	node.SetState(state)
 
-	var err error
-	lifxClient, err = client.New()
+	if nc.BroadcastAddress != "" {
+		lifxClient, err = client.NewWithBroadcastAddress(nc.BroadcastAddress)
+	} else {
+		lifxClient, err = client.New()
+	}
 	if err != nil {
 		log.Error(err)
 		return
