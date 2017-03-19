adb6c9b7a49d7fb7e9d5c76ada771118269b2ff2
Jonathan Grimmtjärn (stamp) <stamp@stamp.se>
2017-03-19 17:48:00 +0100

    Added travis build and release on tags

--- .xc-all.bash
+++ .xc-all.bash
@@ -2,14 +2,24 @@
 
 targets=`go list ./...`
 cwd=`pwd`
+v=$1
+
+[ -z $v ] && exit 1
 
 for target in $targets; do
 	cd $cwd
-
+	
+	#target="$cwd${target/_$cwd\///}"
 	target=${target/github.com\/stampzilla\/stampzilla-go\//}
+
+	echo $target
 	if [ -e "$target/.goxc.json" ] 
 	then
 		cd $target
-		goxc -d "$cwd/build"
+		goxc -d "$cwd/build" -pv=$v
 	fi
 done
+
+
+rename 's/(.*)/$1-arm/' $cwd/build/$v/linux_arm/*
+rename 's/(.*)/$1-amd64/' $cwd/build/$v/linux_amd64/*
