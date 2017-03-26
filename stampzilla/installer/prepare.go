09f3add2213946a97aa7a54de822c75c57cd9199
Jonathan Grimmtjärn (stamp) <stamp@stamp.se>
2017-03-26 11:02:08 +0200

    Fixed missing binary folder

--- stampzilla/installer/prepare.go
+++ stampzilla/installer/prepare.go
@@ -7,7 +7,7 @@ func Prepare() error {
 	CreateUser("stampzilla")
 	CreateDirAsUser("/var/spool/stampzilla", "stampzilla")
 	CreateDirAsUser("/var/log/stampzilla", "stampzilla")
-	CreateDirAsUser("/home/stampzilla/go", "stampzilla")
+	CreateDirAsUser("/home/stampzilla/go/bin", "stampzilla")
 	CreateDirAsUser("/etc/stampzilla", "stampzilla")
 	CreateDirAsUser("/etc/stampzilla/nodes", "stampzilla")
 
