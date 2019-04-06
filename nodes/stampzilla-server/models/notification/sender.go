39baa488ca9233cfc84c2479fb72615ec773278d
Jonathan Grimmtjärn <stamp@stamp.se>
2019-04-06 22:53:16 +0200

    Added destination and senders to server store and redux store

--- nodes/stampzilla-server/models/notification/sender.go
+++ nodes/stampzilla-server/models/notification/sender.go
@@ -1,10 +1,25 @@
 package notification
 
+import "sync"
+
 type Sender interface {
+	ID() string
 	Send(Message) error
 }
 
+type Senders struct {
+	senders map[string]Sender
+	sync.RWMutex
+}
+
+func NewSenders() *Senders {
+	return &Senders{
+		senders: make(map[string]Sender),
+	}
+}
+
 type MailSender struct {
+	ID_      string `json:"id"`
 	Type     Type   `json:"type"`
 	Server   string `json:"server"`
 	Username string `json:"username"`
@@ -12,6 +27,31 @@ type MailSender struct {
 }
 
 type SmsSender struct {
+	ID_    string `json:"id"`
 	Type   Type   `json:"type"`
 	ApiKey string `json:"apiKey"`
+}
+
+func (s *Senders) Add(sender Sender) {
+	s.Lock()
+	s.senders[sender.ID()] = sender
+	s.Unlock()
+}
+
+func (s *Senders) Get(id string) Sender {
+	s.RLock()
+	defer s.RUnlock()
+	return s.senders[id]
+}
+
+func (s *Senders) All() map[string]Sender {
+	s.RLock()
+	defer s.RUnlock()
+	return s.senders
+}
+
+func (s *Senders) Remove(id string) {
+	s.Lock()
+	delete(s.senders, id)
+	s.Unlock()
 }
--- nodes/stampzilla-server/store/store.go
+++ nodes/stampzilla-server/store/store.go
@@ -7,6 +7,7 @@ import (
 	"github.com/stampzilla/stampzilla-go/nodes/stampzilla-server/logic"
 	"github.com/stampzilla/stampzilla-go/nodes/stampzilla-server/models"
 	"github.com/stampzilla/stampzilla-go/nodes/stampzilla-server/models/devices"
+	"github.com/stampzilla/stampzilla-go/nodes/stampzilla-server/models/notification"
 )
 
 type Nodes map[string]*models.Node
@@ -24,6 +25,9 @@ type Store struct {
 	Requests     []Request
 	Server       map[string]map[string]devices.State
 
+	Destinations *notification.Destinations
+	Senders      *notification.Senders
+
 	onUpdate []UpdateCallback
 	sync.RWMutex
 }
@@ -39,6 +43,8 @@ func New(l *logic.Logic, s *logic.Scheduler, sss *logic.SavedStateStore) *Store 
 		Certificates: make([]Certificate, 0),
 		Requests:     make([]Request, 0),
 		Server:       make(map[string]map[string]devices.State),
+		Destinations: notification.NewDestinations(),
+		Senders:      notification.NewSenders(),
 	}
 
 	l.OnReportState(func(uuid string, state devices.State) {
