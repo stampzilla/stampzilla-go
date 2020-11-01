09d09e14c7544b811be11d02856ddfa963ea6ddb
Jonathan Grimmtjärn <stamp@stamp.se>
2020-11-01 22:03:03 +0100

    Rename login to allowLogin in serverinfo model

--- nodes/stampzilla-server/models/serverinfo.go
+++ nodes/stampzilla-server/models/serverinfo.go
@@ -1,10 +1,10 @@
 package models
 
 type ServerInfo struct {
-	Name    string `json:"name"`
-	UUID    string `json:"uuid"`
-	TLSPort string `json:"tlsPort"`
-	Port    string `json:"port"`
-	Init    bool   `json:"init"`
-	Login   bool   `json:"login"`
+	Name       string `json:"name"`
+	UUID       string `json:"uuid"`
+	TLSPort    string `json:"tlsPort"`
+	Port       string `json:"port"`
+	Init       bool   `json:"init"`
+	AllowLogin bool   `json:"allowLogin"`
 }
