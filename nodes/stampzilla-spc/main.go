b136497dd0d137c8d96613d1ecf9515aec85d5c5
Jonas Falck <jonas.falck@gmail.com>
2020-11-04 22:28:51 +0100

    Reconnect on port change
    Graceful shutdown
    support for zones and arm/disarm areas

--- nodes/stampzilla-spc/main.go
+++ nodes/stampzilla-spc/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"context"
+	"encoding/hex"
 	"encoding/json"
 	"fmt"
 	"net"
@@ -8,113 +10,128 @@ import (
 	"sync"
 
 	"github.com/sirupsen/logrus"
-	"github.com/stampzilla/stampzilla-go/nodes/stampzilla-server/models/devices"
+	"github.com/stampzilla/stampzilla-go/nodes/stampzilla-spc/edp"
 	"github.com/stampzilla/stampzilla-go/pkg/node"
 )
 
 func main() {
 	node := node.New("spc")
-	node.OnConfig(updatedConfig)
+	connectToPort := make(chan string)
+	node.OnConfig(updatedConfig(connectToPort))
 	err := node.Connect()
 	if err != nil {
 		logrus.Error(err)
 		return
 	}
 
-	wg := &sync.WaitGroup{}
-	wg.Add(1)
-
-	l, err := net.ListenPacket("udp4", ":8080")
-	if err != nil {
-		fmt.Println(err)
-		logrus.Error(err)
-		return
-	}
-
-	go func() {
-		defer wg.Done()
-		for {
-			buf := make([]byte, 1024)
-			n, _, err := l.ReadFrom(buf)
-			if err != nil {
-				if strings.Contains(err.Error(), "use of closed network connection") {
-					return
-				}
-				logrus.Error(err)
-				continue
-			}
-			// logrus.Infof("read %d from %s\n", n, addr)
-			// logrus.Info("string", string(buf[0:n]))
-			// fmt.Println(hex.Dump(buf[0:n]))
-			// fmt.Println(printHex(buf[0:n]))
-			edp, err := decode(buf[0:n])
-			if err != nil {
-				logrus.Error(err)
-				continue
-			}
-
-			dev := node.GetDevice(edp.ID)
-			state := devices.State{}
-
-			switch edp.Action {
-			case "open":
-				// TODO call it on? or open? or triggered? or active? :)
-				state["on"] = true
-			case "close":
-				state["on"] = false
-			}
-
-			if dev == nil {
-				node.AddOrUpdate(&devices.Device{
-					Type: "sensor",
-					ID: devices.ID{
-						ID: edp.ID,
-					},
-					Name:   edp.Name,
-					Online: true,
-					State:  state,
-				})
-
-				continue
-			}
-
-			node.UpdateState(edp.ID, state)
-		}
-	}()
+	ctx, cancel := context.WithCancel(context.Background())
+	wg := startListen(ctx, node, connectToPort)
 
 	node.OnShutdown(func() {
-		l.Close()
+		cancel()
 	})
 
 	node.Wait()
 	wg.Wait()
 }
 
-// func listenUDP(ctx context.Context) error {
+func startListen(ctx context.Context, node *node.Node, connectToPort chan string) *sync.WaitGroup {
+	wg := &sync.WaitGroup{}
 
-//}
+	listen := func(port string) net.PacketConn {
+		logrus.Infof("started udp4 server on %s", port)
+		l, err := net.ListenPacket("udp4", ":"+port)
+		if err != nil {
+			logrus.Error(err)
+			return nil
+		}
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			for {
+				buf := make([]byte, 1024)
+				n, _, err := l.ReadFrom(buf)
+				if err != nil {
+					if strings.Contains(err.Error(), "use of closed network connection") {
+						return
+					}
+					logrus.Error(err)
+					continue
+				}
+				logrus.Debug("string", string(buf[0:n]))
+				if logrus.GetLevel() >= logrus.DebugLevel {
+					fmt.Println(hex.Dump(buf[0:n]))
+				}
+				pkg, err := edp.Decode(buf[0:n])
+				if err != nil {
+					logrus.Error(err)
+					continue
+				}
+
+				dev := node.GetDevice(pkg.ID)
+				newDev := edp.GenerateDevice(pkg)
+
+				if dev == nil && newDev != nil {
+					node.AddOrUpdate(newDev)
+					continue
+				}
+
+				if newDev == nil {
+					logrus.Warnf("unsupported packet class %s data: %s", pkg.Class, string(buf[23:]))
+					continue
+				}
+
+				node.UpdateState(pkg.ID, newDev.State)
+			}
+		}()
+		return l
+	}
+
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		var l net.PacketConn
+		defer func() {
+			if l != nil {
+				l.Close()
+			}
+		}()
+		for {
+			select {
+			case port := <-connectToPort:
+				if l != nil {
+					l.Close()
+				}
+				l = listen(port)
+			case <-ctx.Done():
+				return
+			}
+		}
+	}()
+
+	return wg
+}
 
 var config = &Config{}
 
-func updatedConfig(data json.RawMessage) error {
-	logrus.Info("Received config from server:", string(data))
+func updatedConfig(connectToPort chan string) node.OnFunc {
+	return func(data json.RawMessage) error {
+		logrus.Info("Received config from server:", string(data))
 
-	newConf := &Config{}
-	err := json.Unmarshal(data, newConf)
-	if err != nil {
-		return err
+		newConf := &Config{}
+		err := json.Unmarshal(data, newConf)
+		if err != nil {
+			return err
+		}
+
+		if newConf.EDPPort != config.EDPPort {
+			fmt.Println("ip changed. lets connect to that instead")
+			logrus.Infof("got new EDPPort from config %s", newConf.EDPPort)
+			connectToPort <- newConf.EDPPort
+		}
+		config = newConf
+		return nil
 	}
-
-	// example when we change "global" config
-	if newConf.EDPPort != config.EDPPort {
-		fmt.Println("ip changed. lets connect to that instead")
-		// TODO stop udp server and start it on new port
-	}
-
-	config = newConf
-	logrus.Info("Config is now: ", config)
-
-	return nil
 }
 
 type Config struct {
@@ -124,7 +141,7 @@ type Config struct {
 /*
 Config to put into gui:
 {
-	EDPPort: "5432"
+	EDPPort: "1234"
 }
 
 */
